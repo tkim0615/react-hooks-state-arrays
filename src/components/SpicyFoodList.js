ff6a45bc59a79ffd7d271cd1f74e689c69013a5e
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-19 17:20:57 -0500

    prep

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,22 +3,56 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    setFoods([...foods, newFood])
+  }
+  const handleClick = (clickedFood) =>{
+    let newFoodArray = foods.map(food =>{
+      if(food.id === clickedFood.id){
+        return {...food, heatLevel: food.heatLevel + 2}
+      }else{
+        return food
+      }
+    })
+    setFoods(newFoodArray)
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+
+  const handleChange =(e) =>{
+    setSelectedCategory(e.target.value)
+  }
+
+  let filter = foods.filter(food=>{
+    if(selectedCategory === "All"){
+      return true
+  }else{
+    return selectedCategory === food.cuisine
+  }
+})
+
+  const foodList = filter.map((food) => (
+    <li onClick = {()=>handleClick(food)} key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+
+
+
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select onChange={handleChange} name="filter">
+  <option value="All">All</option>
+  <option value="American">American</option>
+  <option value="Sichuan">Sichuan</option>
+  <option value="Thai">Thai</option>
+  <option value="Mexican">Mexican</option>
+</select>
     </div>
   );
 }
